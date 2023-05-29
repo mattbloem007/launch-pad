dc8207900ec0b4f45afd1ab6d95c6c8563e7e2bb
nuwrldnf8r <nuwrldnf8r@gmail.com>
2023-05-29 14:22:11 +0200

    Add files via upload

--- src/lib/contractAddresses.js
+++ src/lib/contractAddresses.js
@@ -1,6 +1,6 @@
 const $ = {
-    toaAddress: '0xD498eF589059C117b558fEF3cBaFeb49fe905765',
-    crowdsaleAddress: '0xCbE77C22De85fcfCEd48F0e26212AcA7bEEAc703',
+    toaAddress: '0x600461531E7ae0640df15778d95F836141Fa9110',
+    crowdsaleAddress: '0x95D3E10292c54A9f061AE4F9436FA3c1A71e0E3b',
     USDC: '0x9b76deD4C2386E214dB5B6B70Dd26c37abf39E13',
     Elys: '0x52f1f3d2f38bdbe2377cda0b0dbeb993dc242b98',
     tradepool: ''
