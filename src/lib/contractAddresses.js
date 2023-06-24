de28ee604eb6d2f89404afda6cb8ce32eeef6e73
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2023-06-24 14:07:17 +0200

    new addresses

--- src/lib/contractAddresses.js
+++ src/lib/contractAddresses.js
@@ -1,6 +1,6 @@
 const $ = {
-    toaAddress: '0x31a6223Aa7738721f7B9bDBdDa76049ed9802a77',
-    crowdsaleAddress: '0xAE4Ff484B382636De304F2864B5F3dc29019287B',
+    toaAddress: '0x3f7376977b41dbffed4cf2fB0027a5a31B8600d6',
+    crowdsaleAddress: '0xFdc80eCA6104e72D8ABe71a25c8572D91783ebf2',
     USDC: '0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E',
     Elys: '0xd89cc0d2A28a769eADeF50fFf74EBC07405DB9Fc',
     tradepool: ''
